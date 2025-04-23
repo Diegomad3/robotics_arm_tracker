40a927f23d342fe5eddae6a3e755197897a0e612
Diegomad3 <150369339+Diegomad3@users.noreply.github.com>
2025-04-22 19:26:42 -0500

    Delete ros_arm_vision/server.bat

--- ros_arm_vision/server.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM Get WSL IP address
-FOR /F "delims=" %%i IN ('wsl hostname -I') DO SET WSL_IP=%%i
-echo Detected WSL IP: %WSL_IP%
-
-REM Start FFmpeg stream
-ffmpeg -f dshow -video_size 1280x720 -framerate 30 -vcodec mjpeg -i video="Logi C270 HD WebCam" -vcodec copy -fflags nobuffer -flags low_delay -f mjpeg udp://172.23.246.5:8080?pkt_size=1316
